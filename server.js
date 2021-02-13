9feea91f351a42b9b35526d947c0d17db7757f38
gregor-gh <gregor@live.co.uk>
2021-02-13 00:15:08 +0000

    Added datetime API

--- server.js
+++ server.js
@@ -20,13 +20,37 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", function (_req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp",(_req,res) => {
+  const utc = new Date();
+  const unix = Date.parse(new Date());
 
+  res.json({unix: unix, utc: utc});
+})
+
+app.get("/api/timestamp/:datetime",(req,res) => {
+
+  const value = req.params.datetime
+  let utc;
+  let unix;
+
+  if (!isNaN(value)) {
+    unix = value;
+    utc = new Date(parseInt(value));
+  }
+  else {
+    utc=new Date(value)
+    unix=Date.parse(value)
+  }
+  if (utc=="Invalid Date")
+    res.json({error: "Invalid Date"});
+  else
+    res.json({unix: unix, utc: utc.toUTCString()});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
